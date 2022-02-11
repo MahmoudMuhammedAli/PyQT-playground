617452b99b9543491f4a20d958141613c0199ced
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-02-11 14:16:55 +0200

    yaaaay!

---
+++ src/ftry.py
@@ -0,0 +1,23 @@
+# simple pyqt5 example with a lable and a button
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QApplication , QMainWindow
+import sys
+
+def window():
+  app = QApplication(sys.argv)
+  win = QMainWindow()
+  win.setGeometry(100,100,500,300)
+  win.setWindowTitle('PyQt5 Example')
+  label = QtWidgets.QLabel(win)
+  label.setText('This is a PyQt5 Example')
+  label.move(100,70)
+  button = QtWidgets.QPushButton(win)
+  button.setText('Click Me')
+  button.move(100,100)
+  win.show()
+  sys.exit(app.exec_())
+
+window()
+
+
